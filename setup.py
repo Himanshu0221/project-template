7b4bfe06141b7708085865762f354714d2342300
Himanshu0221 <himanshu_tagare@yahoo.in>
2024-04-22 14:21:27 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chatbot",
+    version="0.0.1",
+    author="himanshu",
+    author_email="himanshu_tagare@yahoo.in",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
